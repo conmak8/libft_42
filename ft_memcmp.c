1da25097db335c8ad6e9d39ee5b5be1338e45095
Constantinos Makariou <cmakario@3-C-3.42heilbronn.de>
2023-10-24 23:56:48 +0200

    android test2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:08:56 by cmakario          #+#    #+#             */
-/*   Updated: 2023/10/24 23:51:52 by cmakario         ###   ########.fr       */
+/*   Updated: 2023/10/24 23:56:38 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,4 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 }
 //top try
 //pull try tablet
+//how is done magic
